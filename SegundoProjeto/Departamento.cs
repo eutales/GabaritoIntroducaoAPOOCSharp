09f7e2ca194cb5130a9a5e3f6e17fc809ffdd01f
Tales Augusto Siqueira <eu.tsiqueira@gmail.com>
2026-05-05 20:01:38 -0300

    Pausa - Slide 26 - Cap 3

---
+++ SegundoProjeto/Departamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoProjeto
+{
+    class Departamento
+    {
+        public Departamento(string nome)
+        {
+            this.Nome = nome;
+        }
+        public string Nome { get; set; }
+    }
+}
---
+++ SegundoProjeto/Program.cs
@@ -0,0 +1,43 @@
+﻿using SegundoProjeto;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var iesUTFPR = new Instituicao();
+        iesUTFPR.Nome = "UTFPR";
+        iesUTFPR.Endereco = "Medianeira";
+
+        var iesCC = new Instituicao();
+        iesCC.Nome = "Casa do Código";
+        iesCC.Endereco = "São Paulo";
+
+        var dptoEnsino = new Departamento("Computação");
+        
+        var dptoAlimentos = new Departamento("Alimentos");
+        
+        var dptoRevisao = new Departamento("Revisão Escrita");
+        
+
+        iesUTFPR.RegistrarDepartamento(dptoEnsino);
+        iesUTFPR.RegistrarDepartamento(dptoAlimentos);
+        iesCC.RegistrarDepartamento(dptoRevisao);
+
+        Console.WriteLine("UTFPR");
+        for (int i = 0; i < iesUTFPR.ObterQuantidadeDepartamentos(); i++)
+        {
+            Console.WriteLine($"=> {iesUTFPR.Departamentos[i].Nome}");
+        }
+
+        Console.WriteLine("Casa do Código");
+        for (int i = 0; i < iesCC.ObterQuantidadeDepartamentos(); i++)
+        {
+            Console.WriteLine($"=> {iesCC.Departamentos[i].Nome}");
+        }
+
+        Console.Write("Pressione qualquer tecla para continuar");
+        Console.ReadKey();
+
+
+    }
+}
